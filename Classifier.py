2eae178c487d0d0e48d6291cf932974b475bafc9
Marie Ribeiro <marie.gita@gmail.com>
2020-07-04 17:19:22 +0100

    Changed architecture of network

---
+++ Classifier.py
@@ -0,0 +1,105 @@
+# LSTM for sequence classification for HOMUS dataset
+from numpy.random import seed #trying to have consistent results. both are apparently needed
+seed(1)
+import tensorflow as tf
+import keras as keras
+tf.random.set_seed(1)
+from keras.layers import Activation
+from sklearn.model_selection import train_test_split
+from keras.datasets import imdb
+from keras.layers import Dense, Conv2D, LSTM, MaxPooling2D, Dropout, Flatten, Reshape, Dense, Input
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from keras import optimizers
+from numpy import *
+from tensorflow.keras import layers
+from keras.layers import TimeDistributed
+from keras.utils.np_utils import to_categorical
+from keras import backend as K
+from keras.layers.merge import concatenate
+from keras.models import Model, Sequential
+import pandas as pd 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from preprocessing import tokenize_vectorize
+
+#Version should be 2.0.0
+print(tf.version.VERSION)
+
+#Data stats:
+#Number of true entries; 21,417
+#Number of false entries; 23,481
+#Total entries; 44,898
+MAX_SEQUENCE_LENGTH = 500
+
+csvFake = pd.read_csv('Fake.csv')
+csvTrue = pd.read_csv('True.csv')
+textFake_df = pd.DataFrame(csvFake[['title','text']])
+textFake_df['label'] = 0  #adding label column to dataframe
+textTrue_df = pd.DataFrame(csvTrue[['title','text']])
+textTrue_df['label'] = 1  #adding labels
+
+frames = [textTrue_df, textFake_df]
+dataset = pd.concat(frames) #merging real and fake dataset
+#This Shuffles the dataset and splits into 80% for training and 20% for testing 
+#as a result, train set = 35,918 and test set = 8,980. Random_state sets seed for splitting 
+trainTexts, testTexts = train_test_split(dataset, test_size=0.2, random_state=1)
+X_train, X_test, Y_train, Y_test, word_index_text, word_index_title = tokenize_vectorize(trainTexts, testTexts)
+text_vocab_size = len(word_index_text) + 1
+title_vocab_size = len(word_index_title) + 1
+
+#Network
+def networkForArticalText():
+    global MAX_SEQUENCE_LENGTH
+    #Takes genre of article and adds to first model
+    model1_in = Input(shape=(MAX_SEQUENCE_LENGTH,))
+    model1_layer1 = Embedding(input_dim=text_vocab_size, output_dim=100, input_length=(MAX_SEQUENCE_LENGTH,))(model1_in)#Dense(40, activation='relu')(model1_in)
+    model1_layer2 = Flatten()(model1_layer1)
+    model1_layer3 = Dense(2, activation='relu')(model1_layer2)
+    model1_layer4 = Dense(2, activation='relu')(model1_layer3)
+    model1_out = Dense(1, activation='relu')(model1_layer4)
+    model1 = Model(model1_in, model1_out)
+    return(model1)
+
+def networkForTitle():
+    global MAX_SEQUENCE_LENGTH
+    model2_in = Input(shape=(MAX_SEQUENCE_LENGTH,))
+    model2_layer1 = Embedding(input_dim=title_vocab_size, output_dim=100, input_length=(MAX_SEQUENCE_LENGTH,))(model2_in)
+    model2_layer2 = LSTM(2, activation='relu',
+                      return_sequences=True, dropout=0.1, stateful=False, name='layer2Title')(model2_layer1)
+    model2_out = Flatten()(model2_layer2)
+    model2 = Model(model2_in, model2_out)
+    return(model2) 
+
+def networksCombined(model1, model2):
+    #Combines 2 models and gives output
+    concatenated = concatenate([model1.output, model2.output])
+    out = Dense(2, activation='softmax', name='output')(concatenated)
+    model = Model([model1.input, model2.input], outputs = out)
+    return(model)
+    
+model1 = networkForArticalText()
+model2 = networkForTitle()
+model = networksCombined(model1, model2)
+model.compile(loss='binary_crossentropy',
+              optimizer=tf.keras.optimizers.SGD(learning_rate=0.1),#
+              #optimizer='Adam', #optimizer only changed to solve userwarning message about 'sparse indexed slices' that occurs with using the embedding layer
+              metrics=['accuracy']) 
+model.fit(
+    X_train,
+    Y_train,
+    epochs = 10, validation_split=0.1,
+    batch_size = 100,
+    verbose = 2 #Removes warnings
+)
+
+#Testing
+scores = model.evaluate(X_test, Y_test, batch_size = 100, verbose=2)
+print("Accuracy: %.2f%%" % (scores[1]*100), flush=True)
+    
+model_json = model.to_json()
+with open("classifierFakeRealNews.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("classifierFakeRealNews.h5")
+print("Saved model to disk", flush=True)
+#Once complete: make java app for use
--- preprocessing.py
+++ preprocessing.py
@@ -1,62 +1,61 @@
 import pandas as pd 
-import numpy as np
+import numpy as numpy
 from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.preprocessing import sequence
 from tensorflow.python.keras.preprocessing import text
 from tensorflow.keras.utils import to_categorical
 
+#Preprocessing Data
 #Parameters
 #Limit on the number of features. Using the top 20,000 features
 TOP_K = 20000
 #Limit on the length of text sequences, longer sequences will be truncated
 MAX_SEQUENCE_LENGTH = 500
 
-#Data stats:
-#Number of true entries; 21,417
-#Number of false entries; 23,481
-#Total entries; 44,898
-
-csvFake = pd.read_csv('Fake.csv') 
-csvTrue = pd.read_csv('True.csv')
-textFake_df = pd.DataFrame(csvFake['text'])
-textFake_df['label'] = 0  #adding label column to dataframe
-textTrue_df = pd.DataFrame(csvTrue['text'])
-textTrue_df['label'] = 1  #adding labels
-
-frames = [textTrue_df, textFake_df]
-dataset = pd.concat(frames) #merging real and fake dataset
-
-#This Shuffles the dataset and splits into 80% for training and 20% for testing 
-#as a result, train set = 35,918 and test set = 8,980
-trainTexts, testTexts = train_test_split(dataset, test_size=0.2)
-#print(trainTexts.head())
-
 def tokenize_vectorize(trainTexts, testTexts):
     #Tokenization and Vectorisation for sequence models, this method assumes that order of words is important in text, and is better for CNN and RNN
     #Create vocabulary with training texts
-    tokenizer = text.Tokenizer(num_words=TOP_K)
+    tokenizer = text.Tokenizer(num_words=TOP_K, lower=False)
     tokenizer.fit_on_texts(trainTexts.text)
-    word_index = tokenizer.word_index
+    word_index_text = tokenizer.word_index
+    #Create vocabulary with training title
+    tokenizer.fit_on_texts(trainTexts.title)
+    word_index_title = tokenizer.word_index    
     #Vectorize the training and validation texts
-    trainSet = tokenizer.texts_to_sequences(trainTexts.text)
-    testSet = tokenizer.texts_to_sequences(testTexts.text)
+    trainSetText = tokenizer.texts_to_sequences(trainTexts.text)
+    testSetText = tokenizer.texts_to_sequences(testTexts.text)
+    trainSetTitle = tokenizer.texts_to_sequences(trainTexts.title)
+    testSetTitle = tokenizer.texts_to_sequences(testTexts.title)    
+    
     #Get max sequence length
-    max_length = len(max(trainSet, key =len))
+    max_length = len(max(trainSetText, key =len))
     if max_length > MAX_SEQUENCE_LENGTH:
         max_length = MAX_SEQUENCE_LENGTH
-
+        
     #Fix sequence length to max value. 
     #The sequence is padded in the beginning if shorter than the length
     #and longer sequences are truncated
-    X_train = sequence.pad_sequences(trainSet, maxlen = max_length)
-    X_test = sequence.pad_sequences(testSet, maxlen = max_length)
-    #print(trainSet)
+    trainSetText = sequence.pad_sequences(trainSetText, maxlen = max_length)
+    trainSetTitle = sequence.pad_sequences(trainSetTitle, maxlen = max_length)
+    testSetText = sequence.pad_sequences(testSetText, maxlen = max_length)
+    testSetTitle = sequence.pad_sequences(testSetTitle, maxlen = max_length)
+    trainSetText = numpy.array(trainSetText)
+    trainSetTitle = numpy.array(trainSetTitle)
+    testSetText = numpy.array(testSetText)
+    testSetTitle = numpy.array(testSetTitle)
+
+    #Commented out to fit with other changes to the classifier model
+    #trainSetText =  trainSetText.reshape((trainSetText.shape[0], trainSetText.shape[1], 1))
+    #testSetText =  testSetText.reshape((testSetText.shape[0], testSetText.shape[1], 1))
+    #trainSetTitle = trainSetTitle.reshape((trainSetTitle.shape[0], trainSetTitle.shape[1], 1))
+    #testSetTitle =  testSetTitle.reshape((testSetTitle.shape[0], testSetTitle.shape[1], 1))
+    #Shape should be 35918, 500, 1
+
+    X_train = [trainSetText, trainSetTitle]
+    X_test = [testSetText, testSetTitle]
 
     #Labels- Converting labels to binary vectors  
     Y_train = to_categorical(trainTexts.label, num_classes=2)
     Y_test = to_categorical(testTexts.label, num_classes=2)
 
-    return X_train, X_test, Y_train, Y_test, word_index
-
-X_train, X_test, Y_train, Y_test, word_index = tokenize_vectorize(trainTexts, testTexts)
-#print(X_train)
+    return X_train, X_test, Y_train, Y_test, word_index_text, word_index_title
